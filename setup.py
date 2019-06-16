be86b32f97ded8fe97ebb8c54b7dc5bc5915460e
Anton Nanchev <ananchev@gmail.com>
2019-06-16 21:11:42 +0200

    updates in setup.py before publish on pypi

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     ],
     include_package_data=True,
     classifiers=(
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
